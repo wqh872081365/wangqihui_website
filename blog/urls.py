444b70c3a5a3a22dcd3e4a3347ba064956c704d9
wangqihui <wangqihui0324@gmail.com>
2016-10-15 17:32:43 +0800

    views edit

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from . import tests
 # from django.contrib import admin
 
 urlpatterns = [
-    url(r'index/$', views.IndexView.as_view(), name='index'),
-    url(r'^test/', tests.test_add),
-    url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
-    url(r'^category/(?P<cate_id>\d+)$', views.CategoryView.as_view(), name='category'),
+    url(r'index/$', views.IndexView.as_view(), name='blog_index'),
+    url(r'test/', tests.test_add),
+    url(r'article/(?P<article_id>\d+)/$', views.ArticleDetailView.as_view(), name='blog_article_detail'),
+    url(r'category/(?P<category_id>\d+)/$', views.CategoryView.as_view(), name='blog_category'),
 ]
