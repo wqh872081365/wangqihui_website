014f4e000ba75025323335040acbdfd5ed356db3
wangqihui <wangqihui0324@gmail.com>
2016-10-25 17:35:24 +0800

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from models import Article,
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    # list_display = ("full_name", "phone", "is_active")
+
+
+
+
