2bec359296e0ff4a664d19da83bd09aa296ff180
wangqihui <wangqihui0324@gmail.com>
2016-10-26 11:10:16 +0800

    admin add

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,22 @@
+# -*- coding:utf-8 -*-
+
 from django.contrib import admin
-from models import Article,
+from models import Article, Category, Tag
 # Register your models here.
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    # list_display = ("full_name", "phone", "is_active")
+    list_display = ("title", "body", "status", "abstract", "views", "likes", "topped", "category")
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
 
 
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -47,9 +47,12 @@ class Article(models.Model):
 
     class Meta:
         ordering = ['-last_modified_time']
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
 
 
 class Category(models.Model):
+
     name = models.CharField('类名', max_length=20, unique=True)
     created_time = models.DateTimeField('创建时间', auto_now_add=True)
     last_modified_time = models.DateTimeField('修改时间', auto_now=True)
@@ -57,16 +60,22 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = '分类'
+
 
 class Tag(models.Model):
-    """
-    tag(标签)对应的数据库model
-    """
+
     name = models.CharField('标签名', max_length=20, unique=True)
     created_time = models.DateTimeField('创建时间', auto_now_add=True)
     last_modified_time = models.DateTimeField('修改时间', auto_now=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = '标签'
 
 
